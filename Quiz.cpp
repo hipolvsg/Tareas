287f2540826425f3f727af1e3ac01e859e8853b4
David Alvarado Navarrete <oscard.alvaradon@gmail.com>
2026-08-12 07:36:47 -0500

    Add files via upload

---
+++ Quiz.cpp
@@ -0,0 +1,19 @@
+/*
+entrada 
+mayor = 17
+menor = 17
+suma = 0
+14 15 17 18 19 21 25
+primera iteracion suma = 17
+mayor = 17 
+menor = 17
+
+
+
+
+salida 
+edad mayor: 25
+edad menor: 14
+promedio: 16.125
+
+*/
