6abaae95f5da295632dfad63399be61c27564e12
David Alvarado Navarrete <oscard.alvaradon@gmail.com>
2026-09-09 08:30:33 -0500

    Update bsuqueda.py

--- Corte2/bsuqueda.py
+++ Corte2/bsuqueda.py
@@ -3,9 +3,11 @@ def busqueda_binaria(arr, x):
     low = 0
     high = len(arr) - 1
     mid = 0
+    global comparaciones_binaria
 
     while low <= high:
         mid = (high + low) // 2
+        comparaciones_binaria += 1
         if arr[mid] < x:
             low = mid + 1
         elif arr[mid] > x:
@@ -15,12 +17,16 @@ def busqueda_binaria(arr, x):
     return -1
 #Arreglo con busqueda secuencial
 def busqueda_secuencial(arr, x):
+    global comparaciones_secuencial
     for i in range(len(arr)):
+        comparaciones_secuencial += 1
         if arr[i] == x:
             return i
     return -1
 #main
 #incializacion de arreglo
+comparaciones_binaria = 0
+comparaciones_secuencial = 0
 arr = [2, 3, 4, 10, 40]
 x = 10
 #llamada a la funcion de busqueda binaria
@@ -35,3 +41,8 @@ if result != -1:
     print("Elemento encontrado en el indice:", str(result))
 else:
     print("Elemento no encontrado en el arreglo")
+
+#como medimos las comparaciones
+print("\t\tCOMPARACIONES")
+print("Comparaciones busqueda binaria:", comparaciones_binaria)
+print("Comparaciones busqueda secuencial:", comparaciones_secuencial)
